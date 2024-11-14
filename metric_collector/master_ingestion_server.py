0459f57f5f581e756202e43b4962d70aaf145745
jayesh sharma <sjay05305@gmail.com>
2024-11-15 00:38:49 +0530

    metrics probe & master ingestion server files

---
+++ metric_collector/master_ingestion_server.py
@@ -0,0 +1,156 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import List
+from jose import JWTError, jwt
+import psycopg2
+from psycopg2 import pool  # Connection pooling
+import os
+from datetime import datetime
+from fastapi.security import OAuth2PasswordBearer
+
+# FastAPI app initialization
+app = FastAPI()
+
+# JWT token configs: secret key and algorithm
+ENV = os.getenv("ENVIRONMENT", "dev")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv('ALGORITHM')
+
+print(f'Environment: {ENV}')
+
+# OAuth2 scheme for token extraction from Authorization header
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Define database credentials based on environment
+DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "127.0.0.1")
+DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+
+
+# Initialize connection pool (this can be done when the app starts)
+db_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=50,  # Adjust max connections according to your app's traffic and database capacity
+    dbname=DATABASE_NAME,
+    user=DATABASE_USER,
+    password=DATABASE_PASSWORD,
+    host=DATABASE_HOST,
+    port=DATABASE_PORT
+)
+
+# Get a connection from the pool
+
+
+def get_db_connection():
+    try:
+        conn = db_pool.getconn()
+        if conn is None:
+            raise HTTPException(
+                status_code=500, detail="Database connection failed")
+        return conn
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Database connection failed: {str(e)}")
+
+# Release a connection back to the pool
+
+
+def release_db_connection(conn):
+    try:
+        if conn:
+            db_pool.putconn(conn)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to release connection: {str(e)}")
+
+# Token verification function
+
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # TODO: verify the token
+        # TODO: probe identification should be part of jwt token
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise credentials_exception
+
+# Structure of ping results
+
+
+class PingResult(BaseModel):
+    ip_address: str
+    avg_rtt: float
+    packets_sent: int
+    packets_received: int
+    packet_loss: float
+    probe_name: str
+
+# Endpoint to get nodes from the database
+
+
+@app.get("/nodes/")
+async def get_nodes(token: str = Depends(verify_token)):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('SELECT * FROM nodes;')
+        nodes = cursor.fetchall()
+        return {"nodes": nodes}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        cursor.close()
+        release_db_connection(conn)
+
+# Endpoint to accept bulk ping results
+
+
+@app.post("/ping_results/")
+async def add_ping_results(ping_results: List[PingResult], token: str = Depends(verify_token)):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        insert_query = '''
+            INSERT INTO ping_results 
+            (ip_address, avg_rtt, packets_sent, packets_received, packet_loss, probe_name, ping_at_datetime)
+            VALUES (%s, %s, %s, %s, %s, %s, NOW())
+        '''
+
+        # Prepare data for bulk insert
+        data_to_insert = [
+            (
+                result.ip_address,
+                result.avg_rtt,
+                result.packets_sent,
+                result.packets_received,
+                result.packet_loss,
+                result.probe_name
+            )
+            for result in ping_results
+        ]
+
+        # Perform bulk insert
+        cursor.executemany(insert_query, data_to_insert)
+        conn.commit()
+
+        return {"status": "success", "message": "Ping results inserted successfully"}
+
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        cursor.close()
+        release_db_connection(conn)
---
+++ metric_probe/probe.py
@@ -0,0 +1,95 @@
+from ping_util import ping_util
+import requests
+import json
+import os
+from datetime import datetime
+from jose import jwt
+
+# JWT token configs: secret key and algorithm
+# this secret match JWT secret in script
+
+env = os.getenv('ENV', 'dev')  # Defaults to 'dev' if not set
+MASTER_INGESTION_URL = os.getenv('MASTER_INGESTION_URL')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+PROBE_NAME = os.getenv('PROBE_NAME')  # take probe name from env variable
+
+print(f'Probe name: {PROBE_NAME}')
+# if probe name is not available, exit the program
+if PROBE_NAME is None:
+    print("Missing probe name!")
+    exit(1)
+
+print(f'Environment: {env}')
+
+
+def generate_token():
+    payload = {
+        "sub": "my_service",
+        "iat": datetime.utcnow().timestamp()
+    }
+    token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+# Get the token
+token = generate_token()
+print(f"Generated token: {token}")
+
+# Headers with the Authorization token
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+
+def get_nodes_from_api():
+    response = requests.get(f"{MASTER_INGESTION_URL}/nodes/", headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        nodes = data['nodes']
+        return nodes
+    else:
+        return []
+
+
+nodes = get_nodes_from_api()
+
+
+# Check if nodes is None
+if nodes is None:
+    print("No nodes available.")
+else:
+    for node in nodes:
+        ip_address = node[3]
+        # print(f'Pinging {ip_address}...')
+        ping_response = ping_util(ip_address)
+        print(ping_response)
+
+        ping_result = {
+            "ip_address": ip_address,
+            "avg_rtt": ping_response.avg_rtt,
+            "packets_sent": ping_response.packets_sent,
+            "packets_received": ping_response.packets_received,
+            "packet_loss": ping_response.packet_loss,
+            "probe_name": PROBE_NAME
+        }
+
+        try:
+            # Send the ping result data to the API endpoint
+            response = requests.post(f"{MASTER_INGESTION_URL}/ping_results/",
+                                     headers=headers,
+                                     data=json.dumps([ping_result])
+                                     )
+
+            # Check if the request was successful
+            if response.status_code == 200:
+                print(
+                    f"Ping result for {ip_address} successfully sent to API.")
+            else:
+                print(
+                    f"Failed to send ping result for {ip_address}. Status code: {response.status_code}")
+                print(f"Response: {response.text}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error sending ping result for {ip_address}: {e}")
